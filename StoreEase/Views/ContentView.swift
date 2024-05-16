48d2e5a5073d520dd8ef358cc8dd6098bb00cb48
jerrytjahyono <jerry.antoni2003@gmail.com>
2024-05-16 21:47:11 +0700

    feat: view Monitoring Items, Dummy Data and Model

---
+++ StoreEase/Views/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  StoreEase
+//
+//  Created by MacBook Pro on 15/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+       SideNavigationBar()
+    }
+
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(timestamp: Date())
+//            modelContext.insert(newItem)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
+}
+
+#Preview {
+    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+}
---
+++ StoreEase/Views/Stocks/StocksRow.swift
@@ -0,0 +1,35 @@
+//
+//  StocksRow.swift
+//  StoreEase
+//
+//  Created by jerry tri tjahyono on 16/05/24.
+//
+
+import SwiftUI
+
+struct StocksRow: View {
+    var product: Product
+    
+    var body: some View {
+        HStack{
+            Text(product.name)
+                .font(.headline)
+                .fontWeight(.regular)
+                .padding(.leading, 10)
+            Spacer()
+            Text("\(product.stocks)")
+                .font(.headline)
+                .fontWeight(.regular)
+                .padding(.trailing, 10)
+        }
+        .padding(.vertical, 6)
+    }
+}
+
+#Preview {
+    Group {
+        StocksRow(product: products[0])
+        StocksRow(product: products[1])
+    }
+}
+
