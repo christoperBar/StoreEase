f3bb72e3d693a3361fbfdce58425a3aebd1918f6
christoperBar <cbarnaby@student.ciputra.ac.id>
2024-05-19 22:52:16 +0700

    refactor: move function getActivityById

--- StoreEase/Views/History/HistoryList.swift
+++ StoreEase/Views/History/HistoryList.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct HistoryList: View {
     @Environment(ModelData.self) var modelData
-//    @State private var searchText = ""
     @State private var dateFilter = Date()
     @State private var filter = FilterActivity.all
     @State private var inspectorIsShown: Bool = false
     @State private var activityId: Int = -1
-//    @State private var selectedActivity: Activity?
     
-//    var activity: Activity
+    func getActivityById(id: Int?) -> Activity? {
+        guard let id = id else { return nil }
+        return modelData.activities.first { $0.id == id }
+    }
     
     enum FilterActivity: String, CaseIterable, Identifiable {
         case all = "All"
@@ -27,20 +28,15 @@ struct HistoryList: View {
     }
     
     var filteredHistory: [Activity] {
-        activities.filter { activity in
+        modelData.activities.filter { activity in
             let matchesType = filter == .all || filter.rawValue == activity.type.rawValue
             let matchesDate = Calendar.current.isDate(activity.date, equalTo: dateFilter, toGranularity: .day)
             return matchesType && matchesDate
         }
     }
     
-//    var index: Int? {
-//        activities.firstIndex(where: { $0.id == selectedActivity?.id })
-//    }
     
     var body: some View {
-        
-//        List(selection: $selectedActivity){
         List(){
             DatePicker("Date", selection: $dateFilter, displayedComponents: .date)
                 .datePickerStyle(.compact)
@@ -52,9 +48,7 @@ struct HistoryList: View {
                         activityId = activity.id
                     }
             }
-//            .focusedValue(\.selectedActivity, activities[index ?? 0])
         }
-        
         .frame(minWidth: 100)
         .navigationTitle("History")
         .toolbar {
@@ -85,13 +79,7 @@ struct HistoryList: View {
                 Text("Please select history")
             }
         }
-//        .searchable(text: $searchText, prompt: "Search")
     }
-}
-
-func getActivityById(id: Int?) -> Activity? {
-    guard let id = id else { return nil }
-    return activities.first { $0.id == id }
 }
 
 #Preview {
